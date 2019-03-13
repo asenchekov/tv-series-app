3c5a8d556c61863cae7ff0ab25f54280787e03c0
Asen Chekov <asen.chekov@gmail.com>
2019-03-13 15:48:23 +0000

    refracturing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,101 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import './App.css';
-
-import Counter from './Counter';
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import Table from './components/Table';
+import ButtonNext from './components/ButtonNext';
 
 const initialState = {
-  count: 0
+  shows: [],
+  imageLinks: [],
+  page: 1,
+  limit: 5,
+  tableCaption: ''
 };
+
+/* 
+  changeNext() {
+    this.setState({
+      page: this.state.page + 1
+    });
+  }
+
+  changePrevious() {
+    if(this.state.page !== 1)
+      this.setState({
+        page: this.state.page - 1
+      });
+  }
+
+  imageLink(imdb) { 
+    const link = HTTP.get('https://api.themoviedb.org/3/find/'+ imdb, {
+        params: {
+            api_key: 'bcdbbabd42939d9fe3b0800ec18a70cf',
+            external_source: 'imdb_id'
+        }
+    }, (error, result) => {
+        if(error) {
+            console.error(error);
+        } else {
+            return 'https://image.tmdb.org/t/p/w500' + result.data.tv_results[0].poster_path
+        }
+    });
+    console.log(link);
+    return link;
+  }
+
+
+  componentDidMount() {
+    const api_Key = '38760291a0e06beeee9e5a43a2217cea8108ee0dc6d9a2fbcf784ac9aec2bdc3';
+    const showsURL = 'https://api.trakt.tv/shows/trending';
+    HTTP.get(showsURL, {
+      params: {
+          page: this.state.page,
+          limit: this.state.limit
+      },
+      headers: {
+          "Content-type": "application/json",
+          "trakt-api-key": api_Key,
+          "trakt-api-version": 2
+      }
+      }, (error, result) => {
+          if(error) {
+              throw new Meteor.Error(error);
+          } else {
+              this.setState({
+                shows: [...result.data],
+                table: 'Trending'
+              });
+          }
+    });
+  }
+
+  componentDidUpdate() {
+    const api_Key = '38760291a0e06beeee9e5a43a2217cea8108ee0dc6d9a2fbcf784ac9aec2bdc3';
+    const showsURL = 'https://api.trakt.tv/shows/trending';
+    HTTP.get(showsURL, {
+      params: {
+          page: this.state.page,
+          limit: this.state.limit
+      },
+      headers: {
+          "Content-type": "application/json",
+          "trakt-api-key": api_Key,
+          "trakt-api-version": 2
+      }
+      }, (error, result) => {
+          if(error) {
+              console.error(error);
+          } else {
+              this.setState({
+                shows: [...result.data],
+                table: 'Trending'
+              });
+          }
+    });
+  }
+*/
 
 function reducer(state = initialState, action) {
   switch(action.type) {
@@ -31,7 +120,18 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Counter />
+        <Header />
+        <UserInput />
+        <Table
+          shows={this.state.shows}
+          tableCaption={this.state.table}
+          page={this.state.page}
+        />
+        <ButtonNext
+          currentPage={this.state.page}
+          changeNext={this.changeNext}
+          changePrevious={this.changePrevious}
+        />
       </Provider>
     );
   }
