fbc6d6160674087fcbc7ceb10d5dd0428daf7056
Asen Chekov <asen.chekov@gmail.com>
2019-03-13 15:03:27 +0000

    Second version without Meteor

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import './App.css';
+
+import Counter from './Counter';
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+  }
+}
+
+export default App;
