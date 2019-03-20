ce33e61dada8a53869e62851c75268ce4cef293f
Asen Chekov <asen.chekov@gmail.com>
2019-03-20 11:43:59 +0000

    Updates

---
+++ precache-manifest.813c7b1c54aa57037280f21f411a2b24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68f31294e5543370a759",
+    "url": "/static/js/main.24b2d2f9.chunk.js"
+  },
+  {
+    "revision": "2d0dc0ea960adba1c601",
+    "url": "/static/js/2.2189ecf8.chunk.js"
+  },
+  {
+    "revision": "68f31294e5543370a759",
+    "url": "/static/css/main.480c51e5.chunk.css"
+  },
+  {
+    "revision": "49756925399d9ac2a093e3146b5758db",
+    "url": "/index.html"
+  }
+];
